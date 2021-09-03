f938c7c2e07ad1b178a1d4a6516337ea0c79864d
Dante Calderon <mail@dantecalderon.dev>
2021-09-02 23:08:51 -0500

    Add deadline and withdraw functionality

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -10,6 +10,8 @@ contract Staker {
 
     mapping(address => uint256) public balances;
     uint256 public constant threshold = 1 ether;
+    uint256 public deadline = now + 30 seconds;
+    bool public openForWithdraw = false;
 
     constructor(address exampleExternalContractAddress) public {
         exampleExternalContract = ExampleExternalContract(
@@ -27,8 +29,34 @@ contract Staker {
 
     // After some `deadline` allow anyone to call an `execute()` function
     //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    function execute() public payable {
+        bool isDeadlineReached = timeLeft() == 0;
+        if (!isDeadlineReached && address(this).balance > threshold) {
+            exampleExternalContract.complete{value: address(this).balance}();
+        } else {
+            openForWithdraw = true;
+        }
+    }
 
     // if the `threshold` was not met, allow everyone to call a `withdraw()` function
 
+    function withdraw(address payable _to) public payable {
+        require(openForWithdraw, "Contract is not open for withdraw");
+        if (openForWithdraw) {
+            // Withdraw all balance
+            uint256 amount = address(this).balance;
+
+            (bool success, ) = _to.call{value: amount}("");
+            require(success, "Failed to send Ether");
+        }
+    }
+
     // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+
+    function timeLeft() public view returns (uint256) {
+        if (now >= deadline) {
+            return 0;
+        }
+        return deadline - now;
+    }
 }
